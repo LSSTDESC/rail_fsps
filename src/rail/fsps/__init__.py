ba8db8c771e1ffce30d9add1ed459c72e8e4597b
Luca Tortorelli <Luca.Tortorelli@dhcp-visitor-enr-113-19.slac.stanford.edu>
2023-08-02 17:52:06 -0700

    removing ._version from __init__ file

--- src/rail/fsps/__init__.py
+++ src/rail/fsps/__init__.py
@@ -1,4 +1,3 @@
-from ._version import __version__
-
+# from ._version import __version__
 from rail.creation.engines.fsps_photometry_creator import *
 from rail.creation.engines.fsps_sed_modeler import *
